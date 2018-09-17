23e99973f2e10a700bf9536a134dd441ae59389e
Przemyslaw Cygan <cyganprzemyslaw@gmail.com>
2018-09-17 19:07:35 +0200

    Change script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 	Mustache.parse(templateCountry);
 
 
-	var url = 'https://restcountries.eu/rest/v1/name/';
+	var url = 'https://restcountries.eu/rest/v2/name/';
 	var countriesList = document.getElementById('countries');
 	document.getElementById('search').addEventListener('click', searchCountries);
 	//Search country
@@ -21,13 +21,18 @@
 	function showCountriesList(resp) {
 	    countriesList.innerHTML = '';
 	    resp.forEach(function(item) {
+	    	var languages = item.languages;
+	    	var langOutput = '';
+	    	for (var i = 0; i < item.languages.length; i++) {
+	    		langOutput = langOutput + item.languages[i].iso639_1 + ' ';
+	    	}
 		    var country = {
 		    	name: item.name, 
 		    	capital: item.capital, 
 		    	alpha2Code: item.alpha2Code, 
 		    	population: item.population, 
-		    	area: item.area, 
-		    	lang: item.languages
+		    	area: item.area,
+		    	lang: langOutput
 		    };
 			var generatedCountry = Mustache.render(templateCountry, country);
 			countriesList.innerHTML += generatedCountry;
